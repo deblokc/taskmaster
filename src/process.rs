c9c0acdd247d7926425c1c81d32b7d51c6f27180
tnaton <74002970+deblokc@users.noreply.github.com>
2023-06-12 18:39:05 +0200

    it is compiling (with warnings but still)

--- src/process.rs
+++ src/process.rs
@@ -3,6 +3,7 @@ use crate::parsing::Program;
 use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::process::{Child, Command, ExitStatus};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -16,17 +17,17 @@ enum Status {
     FATAL,    //The process could not be started succesfully
 }
 
-pub struct Process<'a> {
-    name: String,         //name of the process
-    pid: u32,             //pid of the process
-    start: Instant,       //process's starting timestamp
-    status: Status,       //status of the process (refer to enum Status)
-    program: &'a Program, //associated program's fully parsed config
-    count_restart: u8,    //number of time the program tried to restart
+pub struct Process {
+    name: String,          //name of the process
+    pid: u32,              //pid of the process
+    start: Instant,        //process's starting timestamp
+    status: Status,        //status of the process (refer to enum Status)
+    program: Arc<Program>, //associated program's fully parsed config
+    count_restart: u8,     //number of time the program tried to restart
 }
 
-impl Process<'_> {
-    fn new(name: String, program: &Program) -> Process {
+impl Process {
+    fn new(name: String, program: Arc<Program>) -> Process {
         let pid = 0;
         let start = Instant::now();
         let status = Status::STARTING;
@@ -42,8 +43,8 @@ impl Process<'_> {
         }
     }
 
-    fn start(&mut self, cmd: &str) -> Child {
-        let command = &mut Command::new(cmd);
+    fn start(&mut self) -> Child {
+        let command = &mut Command::new(self.program.command.clone());
 
         self.status = Status::STARTING;
         let child = command.spawn().expect("Faut wrapper tout ca");
@@ -84,80 +85,87 @@ impl Process<'_> {
     }
 }
 
-fn administrator(mut proc: Process) {
-    let mut process_handle = proc.start(&proc.program.command);
-    proc.start = Instant::now();
+fn administrator(mut proc_ref: Arc<Mutex<Process>>) {
+    if let Ok(mut proc) = proc_ref.lock() {
+        let mut process_handle = proc.start();
+        proc.start = Instant::now();
 
-    loop {
-        match process_handle.try_wait() {
-            Ok(Some(exit_status)) => {
-                if !matches!(proc.status, Status::STOPPED) && !matches!(proc.status, Status::EXITED)
-                {
-                    if matches!(proc.status, Status::STOPPING) {
-                        // if process was stopped manually
-                        proc.status = Status::STOPPED;
-                    }
-                    if matches!(proc.status, Status::STARTING) {
-                        // if process could not start
-                        proc.status = Status::BACKOFF;
-                    }
-                    if proc.need_restart(exit_status) {
-                        let mut process_handle = proc.start(&proc.program.command);
-                        //restart the process
+        loop {
+            match process_handle.try_wait() {
+                Ok(Some(exit_status)) => {
+                    if !matches!(proc.status, Status::STOPPED)
+                        && !matches!(proc.status, Status::EXITED)
+                    {
+                        if matches!(proc.status, Status::STOPPING) {
+                            // if process was stopped manually
+                            proc.status = Status::STOPPED;
+                        }
+                        if matches!(proc.status, Status::STARTING) {
+                            // if process could not start
+                            proc.status = Status::BACKOFF;
+                        }
+                        if proc.need_restart(exit_status) {
+                            process_handle = proc.start();
+                            //restart the process
+                        }
                     }
                 }
+                Ok(None) => {
+                    continue;
+                }
+                Err(e) => {
+                    println!("Error with child : {e}");
+                }
             }
-            Ok(None) => {
-                continue;
+            if matches!(proc.status, Status::STARTING) {
+                if proc.start.elapsed() >= Duration::from_secs(proc.program.startsecs.into()) {
+                    proc.status = Status::RUNNING;
+                }
+            } else if matches!(proc.status, Status::STOPPING) {
+                /* if start_of_stop > proc.program.stopwaitsecs */
+                process_handle.kill();
             }
-            Err(e) => {
-                println!("Error with child : {e}");
-            }
-        }
-        if matches!(proc.status, Status::STARTING) {
-            if proc.start.elapsed() >= Duration::from_secs(proc.program.startsecs.into()) {
-                proc.status = Status::RUNNING;
-            }
-        } else if matches!(proc.status, Status::STOPPING) {
-            /* if start_of_stop > proc.program.stopwaitsecs */
-            process_handle.kill();
         }
     }
 }
 
-pub fn infinity() {
-    let mut programs: HashMap<i16, Vec<Program>> = HashMap::new();
-    let mut priorities: Vec<i16> = Vec::new();
-    let mut process: HashMap<String, Process> = HashMap::new();
+pub fn infinity(programs: &HashMap<u16, Vec<Arc<Program>>>) {
+    //    let mut programs: HashMap<u16, Vec<&Program>> = HashMap::new();
+    let mut process: HashMap<String, Arc<Mutex<Process>>> = HashMap::new();
     let mut thread: Vec<thread::JoinHandle<()>> = Vec::new();
 
-    loop {
-        //(programs, priorities) = parsing(file);
-        priorities.sort();
-        for key in &priorities {
-            if key > &999 {
-                return;
-            }
-            let tmp = match programs.get(key) {
-                // Get the vector of programs associated with this priority
-                Some(x) => x,
-                _ => todo!(),
-            };
-            for proc in tmp {
-                if proc.numprocs == 1 {
-                    let current = Process::new(proc.name.clone(), proc);
-                    process.insert(current.name.clone(), current);
-                    // if only one process use the name given
+    //  loop {
+    //(programs, priorities) = parsing(file);
+    let mut priorities: Vec<&u16> = programs.keys().collect();
+    priorities.sort();
+    for key in priorities {
+        let tmp = programs.get(key).expect("no value for this key");
+        for prog in tmp {
+            if prog.numprocs == 1 {
+                let mut theprocess = Process::new(prog.name.clone(), Arc::clone(prog));
+                let mutex = Mutex::new(theprocess);
+                let current = Arc::new(mutex); //Mutex::new(Process::new(prog.name.clone(), prog)));
+                process.insert(prog.name.clone(), Arc::clone(&current));
+                // if only one process use the name given
+                thread.push(thread::spawn(move || administrator(current)));
+                // Arc<Mutex<Process>>
+            } else {
+                todo!();
+                /*                for i in 0..prog.numprocs {
+                    let current = Arc::new(Mutex::new(Process::new(
+                        prog.name.clone() + &i.to_string(),
+                        prog,
+                    )));
+                    process.insert(prog.name.clone() + &i.to_string(), Arc::clone(&current));
+                    // if multiple process add number after
                     thread.push(thread::spawn(move || administrator(current)));
-                } else {
-                    for i in 0..proc.numprocs {
-                        let current = Process::new(proc.name.clone() + &i.to_string(), proc);
-                        process.insert(current.name.clone(), current);
-                        // if multiple process add number after
-                    }
-                }
-                // Create process and insert them in the map with name as key
+                }*/
             }
+            // Create process and insert them in the map with name as key
         }
+    }
+    // }
+    for child in thread {
+        child.join();
     }
 }
